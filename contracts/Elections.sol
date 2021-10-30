1437752ff559fc3c73d9e5c7e38c7cdb6644bbb0
Dani <danillano97@gmail.com>
2021-10-29 23:41:50 -0300

    vote

--- contracts/Elections.sol
+++ contracts/Elections.sol
@@ -1,12 +1,16 @@
 pragma solidity ^0.5.7;
 
 contract Election {
+    
     // model a candidate
     struct Candidate {
         uint id;
         string name;
         uint voteCount;
     }
+
+    // store accounts that have voted
+    mapping(address => bool) public voters;
 
     // read/write candidate
     mapping(uint => Candidate) public candidates;
@@ -23,6 +27,17 @@ contract Election {
     function addCandidate (string _name) private {
         candidatesCount ++;
         candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+    function vote (uint _candidateId) public {
+        // require that they haven't voted before
+        require(!voters[msg.sender]);
+        // require a valid candidate
+        require(_candidateId > 0 && _candidateId <= candidatesCount);
+        // record that voter has voted
+        voters[msg.sender] = true;
+        // update candidate vote Count
+        candidates[_candidateId].voteCount ++;
     }
 
 }
